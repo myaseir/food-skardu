45e87ca4638b427d45d4cb6e8d6b4eda383981c3
Muhammad Yasir <yasirabbasi0351@gmail.com>
2026-07-29 16:53:09 +0500

    Added

--- data/config.ts
+++ data/config.ts
@@ -46,7 +46,7 @@ export const shops: Shop[] = [
     alwaysOpen: false,
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_s9v8YpPGvHU9y-lQBEtH1KWfYGK93JAqX_L7Zyi5mD_awCYaLjmdxcZ_&s=10",
     lat: 35.289211, lng: 75.631235, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923485825247",
     rating: 4.6,
     reviews: 185,
   },
@@ -73,7 +73,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpO1EPdDa0L41JIdnK45cWhQVFiulx09fx2p5amMNXFXwBXUiOyj8itdI&s=10", // TODO: paste real logo URL
     lat: 35.28872, // TODO: replace with real coordinates
     lng: 75.630066, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923555709276",
     rating: 4.8, // TODO: adjust once real reviews exist
     reviews: 61, // TODO: adjust once real reviews exist
   },
@@ -87,7 +87,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtUeS45GsAE9jnvKmammejtmSPfBAW92Su-zP4Wj8yMw&s=10",
     lat: 35.288921, // TODO: replace with real coordinates
     lng: 75.629178, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923554524401",
     rating: 4.1,
     reviews: 106,
   },
@@ -130,7 +130,7 @@ export const shops: Shop[] = [
     logo: "https://res.cloudinary.com/dxxqrjnje/image/upload/v1784291847/ChatGPT_Image_Jul_17_2026_05_37_05_PM_t6yq3u.jpg",
     lat: 35.2900162, // TODO: replace with real coordinates
     lng: 75.6374406, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923167018580",
     rating: 4.5,
     reviews: 15,
   },
@@ -145,7 +145,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMQ6dmqgrHiuXYkuCAjoK9uflq4-G8JiXB6Fglzqfsw9AAN9KhQRlQ6vs&s=10",
     lat: 35.289893, // TODO: replace with real coordinates
     lng: 75.637079, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923554220114",
     rating: 4.3,
     reviews: 21,
   },
@@ -159,7 +159,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQipAadvX55uIxjvM_YfwT8z-A_IxMxT3Quh1MHxi1g9g&s", // TODO: paste real logo URL
     lat: 35.302132, // TODO: replace with real coordinates
     lng: 75.625344, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923441518777",
     rating: 4.5,          // adjust as needed
     reviews: 46,
   },
@@ -173,7 +173,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa8xtMwcGub4wGh9HgvVns3fAAIMH8V7a5rR3IzlQMijEyMbt9XXL4Rhrv&s=10",
     lat: 35.289174, // TODO: replace with real coordinates
     lng: 75.636453, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923453220824",
     rating: 4.0,
     reviews: 29,
   },
@@ -187,7 +187,7 @@ export const shops: Shop[] = [
     logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTCK1MjSAqQF5vWaFQeoB26t69zQdIJEkEBA8pGEZYAs8QhCwkmFSRmwcG&s=10",
     lat: 35.296598, // TODO: replace with real coordinates
     lng: 75.643991, // TODO: replace with real coordinates
-    whatsapp: "923169030178",
+    whatsapp: "923408922555",
     rating: 4.2,
     reviews: 37,
   },
